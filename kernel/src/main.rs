5d3802c142b86d2c351182babf94bfb352546048
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-05-08 20:40:09 +0300

    kernel: ask limine for a bigger stack size

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -4,7 +4,7 @@
 use core::arch::asm;
 
 use limine::BaseRevision;
-use limine::request::{RequestsEndMarker, RequestsStartMarker};
+use limine::request::{RequestsEndMarker, RequestsStartMarker, StackSizeRequest};
 
 #[used]
 #[unsafe(link_section = ".requests")]
@@ -14,6 +14,12 @@ static BASE_REVISION: BaseRevision = BaseRevision::new();
 #[unsafe(link_section = ".requests_start_marker")]
 static _START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
 
+pub const STACK_SIZE: u64 = 0x100000;
+
+#[used]
+#[unsafe(link_section = ".requests")]
+static STACK_SIZE_REQUEST: StackSizeRequest = StackSizeRequest::new().with_size(STACK_SIZE);
+
 #[used]
 #[unsafe(link_section = ".requests_end_marker")]
 static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
@@ -21,6 +27,10 @@ static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
 #[unsafe(no_mangle)]
 unsafe extern "C" fn entry() -> ! {
     assert!(BASE_REVISION.is_supported());
+
+    if STACK_SIZE_REQUEST.get_response().is_none() {
+        panic!("could not ask limine for bigger stack size");
+    }
 
     hlt();
 }
