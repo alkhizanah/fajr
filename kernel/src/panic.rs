72465f6060b7368ff69513e9229ad4f676c0d187
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-05-13 09:35:39 +0300

    change background of kernel panic, and add instructions to go to the github repository

--- kernel/src/panic.rs
+++ kernel/src/panic.rs
@@ -1,5 +1,9 @@
+use core::fmt::Write;
+
 use crate::arch::endless_loop;
+use crate::console::CONSOLE;
 use crate::requests::FRAMEBUFFER_REQUEST;
+use crate::screen::Color;
 
 #[panic_handler]
 fn panic_handler(info: &core::panic::PanicInfo) -> ! {
@@ -10,7 +14,32 @@ fn panic_handler(info: &core::panic::PanicInfo) -> ! {
         .get_response()
         .is_some_and(|response| response.framebuffers().next().is_some())
     {
-        println!("{}", info);
+        let mut console = CONSOLE.lock();
+
+        console.background = Color::new(0, 128, 255);
+        console.foreground = Color::WHITE;
+
+        console.clear();
+
+        if let Some(location) = info.location() {
+            let _ = writeln!(
+                &mut console,
+                "Panic occured at {} in the kernel's source code",
+                location
+            );
+        } else {
+            let _ = writeln!(
+                &mut console,
+                "Panic occured but can't get the location information"
+            );
+        }
+
+        let _ = writeln!(
+            &mut console,
+            "You can get help by going to the GitHub repository of the kernel, located at github.com/alkhizanah/khazraj",
+        );
+
+        let _ = writeln!(&mut console, "Panic message: {}", info.message());
     }
 
     endless_loop();
