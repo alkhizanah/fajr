f4459775d82e94618cf92072cacf3e4e1ba0203c
Yhya Ibrahim <yhyamobile57@gmail.com>
2025-05-24 21:07:42 +0300

    don't depend on `bitflags` crate

--- kernel/src/arch/x86_64/gdt.rs
+++ kernel/src/arch/x86_64/gdt.rs
@@ -1,7 +1,6 @@
 use core::arch::asm;
 
 use bit_field::BitField;
-use bitflags::bitflags;
 use lazy_static::lazy_static;
 
 use super::{DescriptorTableRegister, tss::TaskStateSegment};
@@ -54,103 +53,91 @@ enum Descriptor {
     SystemSegment(u64, u64),
 }
 
-bitflags! {
-    /// Flags for a GDT descriptor. Not all flags are valid for all descriptor types.
-    #[derive(PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Clone, Copy)]
-    pub struct DescriptorFlags: u64 {
-        /// Set by the processor if this segment has been accessed. Only cleared by software.
-        /// _Setting_ this bit in software prevents GDT writes on first use.
-        const ACCESSED          = 1 << 40;
-        /// For 32-bit data segments, sets the segment as writable. For 32-bit code segments,
-        /// sets the segment as _readable_. In 64-bit mode, ignored for all segments.
-        const WRITABLE          = 1 << 41;
-        /// For code segments, sets the segment as “conforming”, influencing the
-        /// privilege checks that occur on control transfers. For 32-bit data segments,
-        /// sets the segment as "expand down". In 64-bit mode, ignored for data segments.
-        const CONFORMING        = 1 << 42;
-        /// This flag must be set for code segments and unset for data segments.
-        const EXECUTABLE        = 1 << 43;
-        /// This flag must be set for user segments (in contrast to system segments).
-        const USER_SEGMENT      = 1 << 44;
-        /// These two bits encode the Descriptor Privilege Level (DPL) for this descriptor.
-        /// If both bits are set, the DPL is Ring 3, if both are unset, the DPL is Ring 0.
-        const DPL_RING_3        = 3 << 45;
-        /// Must be set for any segment, causes a segment not present exception if not set.
-        const PRESENT           = 1 << 47;
-        /// Available for use by the Operating System
-        const AVAILABLE         = 1 << 52;
-        /// Must be set for 64-bit code segments, unset otherwise.
-        const LONG_MODE         = 1 << 53;
-        /// Use 32-bit (as opposed to 16-bit) operands. If [`LONG_MODE`][Self::LONG_MODE] is set,
-        /// this must be unset. In 64-bit mode, ignored for data segments.
-        const DEFAULT_SIZE      = 1 << 54;
-        /// Limit field is scaled by 4096 bytes. In 64-bit mode, ignored for all segments.
-        const GRANULARITY       = 1 << 55;
+#[allow(dead_code)]
+mod descriptor_flags {
+    /// Set by the processor if this segment has been accessed. Only cleared by software.
+    /// _Setting_ this bit in software prevents GDT writes on first use.
+    pub const ACCESSED: u64 = 1 << 40;
+    /// For 32-bit data segments, sets the segment as writable. For 32-bit code segments,
+    /// sets the segment as _readable_. In 64-bit mode, ignored for all segments.
+    pub const WRITABLE: u64 = 1 << 41;
+    /// For code segments, sets the segment as “conforming”, influencing the
+    /// privilege checks that occur on control transfers. For 32-bit data segments,
+    /// sets the segment as "expand down". In 64-bit mode, ignored for data segments.
+    pub const CONFORMING: u64 = 1 << 42;
+    /// This flag must be set for code segments and unset for data segments.
+    pub const EXECUTABLE: u64 = 1 << 43;
+    /// This flag must be set for user segments (in contrast to system segments).
+    pub const USER_SEGMENT: u64 = 1 << 44;
+    /// These two bits encode the Descriptor Privilege Level (DPL) for this descriptor.
+    /// If both bits are set, the DPL is Ring 3, if both are unset, the DPL is Ring 0.
+    pub const DPL_RING_3: u64 = 3 << 45;
+    /// Must be set for any segment, causes a segment not present exception if not set.
+    pub const PRESENT: u64 = 1 << 47;
+    /// Available for use by the Operating System
+    pub const AVAILABLE: u64 = 1 << 52;
+    /// Must be set for 64-bit code segments, unset otherwise.
+    pub const LONG_MODE: u64 = 1 << 53;
+    /// Use 32-bit (as opposed to 16-bit) operands. If [`LONG_MODE`][LONG_MODE] is set,
+    /// this must be unset. In 64-bit mode, ignored for data segments.
+    pub const DEFAULT_SIZE: u64 = 1 << 54;
+    /// Limit field is scaled by 4096 bytes. In 64-bit mode, ignored for all segments.
+    pub const GRANULARITY: u64 = 1 << 55;
 
-        /// Bits `0..=15` of the limit field (ignored in 64-bit mode)
-        const LIMIT_0_15        = 0xFFFF;
-        /// Bits `16..=19` of the limit field (ignored in 64-bit mode)
-        const LIMIT_16_19       = 0xF << 48;
-        /// Bits `0..=23` of the base field (ignored in 64-bit mode, except for fs and gs)
-        const BASE_0_23         = 0xFF_FFFF << 16;
-        /// Bits `24..=31` of the base field (ignored in 64-bit mode, except for fs and gs)
-        const BASE_24_31        = 0xFF << 56;
-    }
-}
+    /// Bits `0..=15` of the limit field (ignored in 64-bit mode)
+    pub const LIMIT_0_15: u64 = 0xFFFF;
+    /// Bits `16..=19` of the limit field (ignored in 64-bit mode)
+    pub const LIMIT_16_19: u64 = 0xF << 48;
+    /// Bits `0..=23` of the base field (ignored in 64-bit mode, except for fs and gs)
+    pub const BASE_0_23: u64 = 0xFF_FFFF << 16;
+    /// Bits `24..=31` of the base field (ignored in 64-bit mode, except for fs and gs)
+    pub const BASE_24_31: u64 = 0xFF << 56;
 
-impl DescriptorFlags {
-    const COMMON: Self = Self::from_bits_truncate(
-        Self::USER_SEGMENT.bits()
-            | Self::PRESENT.bits()
-            | Self::WRITABLE.bits()
-            | Self::ACCESSED.bits()
-            | Self::LIMIT_0_15.bits()
-            | Self::LIMIT_16_19.bits()
-            | Self::BASE_0_23.bits()
-            | Self::BASE_24_31.bits()
-            | Self::GRANULARITY.bits(),
-    );
+    pub const COMMON: u64 = USER_SEGMENT
+        | PRESENT
+        | WRITABLE
+        | ACCESSED
+        | LIMIT_0_15
+        | LIMIT_16_19
+        | BASE_0_23
+        | BASE_24_31
+        | GRANULARITY;
 
-    const KERNEL_CODE: Self = Self::from_bits_truncate(
-        Self::COMMON.bits() | Self::LONG_MODE.bits() | Self::EXECUTABLE.bits(),
-    );
+    pub const KERNEL_CODE: u64 = COMMON | LONG_MODE | EXECUTABLE;
 
-    const KERNEL_DATA: Self =
-        Self::from_bits_truncate(Self::COMMON.bits() | Self::DEFAULT_SIZE.bits());
+    pub const KERNEL_DATA: u64 = COMMON | DEFAULT_SIZE;
 
-    const USER_CODE: Self =
-        Self::from_bits_truncate(Self::KERNEL_CODE.bits() | Self::DPL_RING_3.bits());
+    pub const USER_CODE: u64 = KERNEL_CODE | DPL_RING_3;
 
-    const USER_DATA: Self =
-        Self::from_bits_truncate(Self::KERNEL_DATA.bits() | Self::DPL_RING_3.bits());
+    pub const USER_DATA: u64 = KERNEL_DATA | DPL_RING_3;
 }
 
 impl Descriptor {
     #[inline]
     pub const fn kernel_code_segment() -> Descriptor {
-        Descriptor::UserSegment(DescriptorFlags::KERNEL_CODE.bits())
+        Descriptor::UserSegment(descriptor_flags::KERNEL_CODE)
     }
 
     #[inline]
     pub const fn kernel_data_segment() -> Descriptor {
-        Descriptor::UserSegment(DescriptorFlags::KERNEL_DATA.bits())
+        Descriptor::UserSegment(descriptor_flags::KERNEL_DATA)
     }
 
     #[inline]
     pub const fn user_code_segment() -> Descriptor {
-        Descriptor::UserSegment(DescriptorFlags::USER_CODE.bits())
+        Descriptor::UserSegment(descriptor_flags::USER_CODE)
     }
 
     #[inline]
     pub const fn user_data_segment() -> Descriptor {
-        Descriptor::UserSegment(DescriptorFlags::USER_DATA.bits())
+        Descriptor::UserSegment(descriptor_flags::USER_DATA)
     }
 
     #[inline]
     pub fn task_state_segment(tss: &'static TaskStateSegment) -> Descriptor {
         let ptr = tss as *const _ as u64;
 
-        let mut low = DescriptorFlags::PRESENT.bits();
+        let mut low = descriptor_flags::PRESENT;
         let mut high = 0;
 
         // address
